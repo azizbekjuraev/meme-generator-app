4247bd3f96e79b663bca622cfe319ef846198acb
AZIZBEKJURAEV <ajuraev98@mail.ru>
2022-12-27 12:10:33 +0500

    added inputValue, changeColorBtn

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import memeData from "../memeData";
+import cx from "classnames";
 
 export default function () {
-  const [isGoingOut, setGoingOut] = useState(true);
-  // const [memeImage, setMemeImage] = useState("");
   const [content, setContent] = useState({
     topText: "",
     bottomText: "",
@@ -21,23 +20,65 @@ export default function () {
       randomImage: url,
     }));
   }
+  // Value from texts in input field
+  const [val, setValue] = useState("");
+  const change = (event) => {
+    setValue(event.target.value);
+  };
+
+  const [val2, setValue2] = useState("");
+  const change2 = (event) => {
+    setValue2(event.target.value);
+  };
+
+  const [isColorBlack, setIsColorBlack] = useState(true);
+
+  function colorChanger() {
+    setIsColorBlack((prev) => !prev);
+  }
 
   return (
     <div className="main-container">
       <div className="form">
-        <input type="text" className="input1-el" placeholder="Top text" />
-        <input type="text" className="input2-el" placeholder="Buttom text" />
+        <input
+          type="text"
+          className="input1-el"
+          placeholder="Top text"
+          onChange={change}
+        />
+        <input
+          type="text"
+          className="input2-el"
+          placeholder="Buttom text"
+          onChange={change2}
+        />
+        <input type="color" className="input-color" />
+        <div className="color-change">
+          <button onClick={colorChanger}>Color Switch</button>
+        </div>
       </div>
       <div className="get-btn">
         <button onClick={getMemeImage}>Get a new meme image 🌠</button>
+        <a href={allMemes.randomImage} download>
+          ⬇️
+        </a>
       </div>
       <div className="img-holder">
         <img src={allMemes.randomImage} />
-        <p className="img-text-1">I work</p>
-        <p className="img-text-2">hard!</p>
-      </div>
-      <div className="state--value">
-        <h1></h1>
+        <p
+          className={cx("img-text-1", {
+            "color-white": !isColorBlack,
+          })}
+        >
+          {val}
+        </p>
+        <p
+          className={cx("img-text-2", {
+            "color-white": !isColorBlack,
+          })}
+        >
+          {val2}
+        </p>
       </div>
     </div>
   );
